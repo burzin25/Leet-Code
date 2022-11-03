230336a606ad26a6fd5413244b6ccca4888b0dc5
Burzin Wadia <84149050+burzin25@users.noreply.github.com>
2022-11-03 10:40:08 -0500

    Time: 34 ms (89.31%), Space: 13.8 MB (80.28%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,7 +1,13 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+        """ 
+        In a sentence of words with multiple spaces,the user wants to find out the length of           the last word in the sentence without counting the unnecessary spaces nect to the last         word.
+        """
         
+        # Step 1: To split the words from the spaces in the sentence and make them individual             elements of a list using the split()
         word_list = s.split()
         
+        
+        #Step 2: After creating a list of words from the input sentence call the list inndexing          methods and input [-1] as the last element/word in the list/sentence
         return len(word_list[-1])
         
